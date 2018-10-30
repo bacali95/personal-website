86a735afef7dd8c8e15f57a128c87c12c64ee244
bacali95 <nasreddine.bacali95@gmail.com>
2018-10-30 18:40:30 +0100

    fix

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -14,6 +14,7 @@ router.get('/', ensureAuthenticated, function (req, res, next) {
 });
 
 router.get('/login', function (req, res, next) {
+    console.log(process.env);
     if (req.isAuthenticated()) {
         return res.redirect('/admin');
     } else {
