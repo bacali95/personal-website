5772e11be12e256746e0bf659c8e396d1ed8aced
bacali95 <nasreddine.bacali95@gmail.com>
2018-10-30 18:00:18 +0100

    fix

--- tools/specs.js
+++ tools/specs.js
@@ -1,6 +1,6 @@
-const DB_USER = process.env.DB_USER || 'test';
-const DB_PASS = process.env.DB_PASS || 'test';
-const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_USER = process.env.DB_USER === '' ? 'test' : process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS === '' ? 'test' : process.env.DB_PASS;
+const DB_HOST = process.env.DB_HOST === '' ? 'localhost' : process.env.DB_HOST;
 module.exports.DB_URL = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}/personal_db`;
 
 module.exports.ADMIN_USERNAME = process.env.ADMIN_USERNAME;
