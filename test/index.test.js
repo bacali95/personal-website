6125d13d9d2c96cba15504283ff7563557958b13
bacali95 <nasreddine.bacali95@gmail.com>
2018-12-04 19:08:35 +0100

    add test mocha

---
+++ test/index.test.js
@@ -0,0 +1,20 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+
+const server = require('../app');
+
+describe('Index Page', function () {
+    it("renders successfully", function (done) {
+        chai.request(server)
+            .get('/book')
+            .end((err, res) => {
+                res.should.have.status(200);
+                done();
+            });
+    })
+});
