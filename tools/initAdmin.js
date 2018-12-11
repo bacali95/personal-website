e514f688a2d2836937666bfbae558a32a4f94d9e
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2018-12-11 23:51:15 +0100

    fix initAdmin

--- tools/initAdmin.js
+++ tools/initAdmin.js
@@ -1,17 +1,9 @@
 const specs = require("../tools/specs");
 const User = require("../models/user");
 
-module.exports = () => {
-    User.getByUsername(specs.ADMIN_USERNAME, function (err, user) {
-        if (err) {
-            throw err;
-        }
-        if (!user) {
-            User.register(new User({username: specs.ADMIN_USERNAME}),specs.ADMIN_PASSWORD, function (err) {
-                if (err) {
-                    throw err;
-                }
-            });
-        }
-    });
+module.exports = async () => {
+    const user = await User.getByUsername(specs.ADMIN_USERNAME);
+    if (!user) {
+        User.create(specs.ADMIN_USERNAME, specs.ADMIN_PASSWORD);
+    }
 };
